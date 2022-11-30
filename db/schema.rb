fb6cbd8e3c127596cce2aadf8310a6779c9296b8
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-11-30 14:50:56 +0900

    モデル実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_040552) do
+ActiveRecord::Schema.define(version: 2022_11_30_052908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2022_11_30_040552) do
     t.string "name"
     t.string "ingredient"
     t.date "to_do_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

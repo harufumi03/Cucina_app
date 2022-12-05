4626462f65bc785a205342bdaec3eb74bcdd8a04
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-12-05 16:49:15 +0900

    modelアソシエーション定義

--- app/models/food_prep.rb
+++ app/models/food_prep.rb
@@ -1,5 +1,5 @@
 class FoodPrep < ApplicationRecord
   has_many :food_prep_labels, dependent: :destroy
   has_many :labels, through: :food_prep_labels, source: :label
-  belongs_to :user
+  has_many :select_food_preps, dependent: :destroy
 end

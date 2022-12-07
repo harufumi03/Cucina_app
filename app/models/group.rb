6c7c104c4b140c8b2fd56e5e761a4f027eff020f
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-12-07 16:20:56 +0900

    グループ機能のモデル修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,11 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
+  has_many :group_users, dependent: :destroy
+  has_many :users, through: :group_users, source: :user
+
+  def invite_member(user)
+    group_users.create(user: user)
+  end
 end

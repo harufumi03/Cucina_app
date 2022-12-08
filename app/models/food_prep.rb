b93246a4a8d04cf193404042223450842bcfd5ca
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-12-08 18:45:21 +0900

    グループの仕込みを表示できるよう修正

--- app/models/food_prep.rb
+++ app/models/food_prep.rb
@@ -2,4 +2,5 @@ class FoodPrep < ApplicationRecord
   has_many :food_prep_labels, dependent: :destroy
   has_many :labels, through: :food_prep_labels, source: :label
   has_many :select_food_preps, dependent: :destroy
+  belongs_to :group
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users, source: :user
+  has_many :food_preps, dependent: :destroy
 
   def invite_member(user)
     group_users.create(user: user)

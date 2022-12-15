536d4425e69756fee03fc6148e9baef66c1a6604
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-12-15 15:52:27 +0900

    デザイン実装

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module CucinaApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+    # config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
